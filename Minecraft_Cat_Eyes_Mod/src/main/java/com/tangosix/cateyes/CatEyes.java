ee634988511fa2086e283ac15f481821a93c7ab0
Tommy Vo <votommy@outlook.com>
2021-09-10 20:34:45 -0500

    Changes to Fabric source code

---
+++ Minecraft_Cat_Eyes_Mod/src/main/java/com/tangosix/cateyes/CatEyes.java
@@ -0,0 +1,36 @@
+package com.tangosix.cateyes;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import org.lwjgl.glfw.GLFW;
+
+public class CatEyes implements ModInitializer {
+
+	private static KeyBinding keyBinding;
+
+	@Override
+	public void onInitialize() {
+
+		keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"Toggle Cat Vision", //the keybinding's name
+				InputUtil.Type.KEYSYM, //KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_V, "key.categories.misc" //the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			while (keyBinding.wasPressed()) {
+				if (client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+					client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+				} else {
+					client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 9600));
+				}
+			}
+		});
+	}
+
+}
