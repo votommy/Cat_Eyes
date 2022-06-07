ecd34a855ed661d9f11d82d1d7dd9949e9d63c14
Tommy Vo <votommy@outlook.com>
2022-06-07 11:40:29 -0500

    1.19 update

---
+++ 1.19/src/main/java/com/tangosix/cateyes/CatEyes.java
@@ -0,0 +1,58 @@
+package com.tangosix.cateyes;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import org.lwjgl.glfw.GLFW;
+
+public class CatEyes implements ModInitializer {
+
+	private static KeyBinding toggleBTN;
+
+	@Override
+	public void onInitialize() {
+		toggleBTN = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+				"Toggle Cat Vision", //the keybinding's name
+				InputUtil.Type.KEYSYM, //KEYSYM for keyboard, MOUSE for mouse.
+				GLFW.GLFW_KEY_V, "key.categories.misc" //the keybinding's category.
+		));
+
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			Runnable runnable = () -> {
+				while (client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+					try {
+						Thread.sleep(5000);
+					}
+					catch (InterruptedException ex) {
+						System.out.println(ex);
+					}
+					if (client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+						client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 420));
+					}
+				}
+			};
+
+			while (toggleBTN.wasPressed()) {
+				if (client.player.hasStatusEffect(StatusEffects.NIGHT_VISION)) {
+					client.player.removeStatusEffect(StatusEffects.NIGHT_VISION);
+				} else {
+					client.player.addStatusEffect(new StatusEffectInstance(StatusEffects.NIGHT_VISION, 420));
+
+					Thread thread = new Thread(runnable);
+					thread.start();
+				}
+			}
+		});
+	}
+
+	public void catVisionTimer(MinecraftClient client) {
+
+
+	}
+
+}
